f36fa4cb113b00f6769c722389d74c66ae455c32
Jonathanmasa <jonathanmasa97@gmail.com>
2024-12-20 09:38:42 +0100

    patch[array1]

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,8 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers.splice(3, 1, 'fourthTeacher');
-console.log(teachers, fourthTeacher);
+const fourthTeacher = teachers.splice[3];
+console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
